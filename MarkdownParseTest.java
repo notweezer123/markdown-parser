0261f59aa4df9739656368ce8ae0a344baa8707a
notweezer123 <efentress@ucsd.edu>
2022-05-22 18:15:44 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -86,7 +86,7 @@ public class MarkdownParseTest {
     @Test
     public void testSnippet1() throws IOException {
         String contents= Files.readString(Path.of("./snippet-1.md"));
-        List<String> expect = List.of("google.com", "google.com", "ucsd.edu");
+        List<String> expect = List.of("google.com");
         assertEquals(expect, MarkdownParse.getLinks(contents));
     }
 
