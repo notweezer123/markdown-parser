0b9f89861ea24f5e94887d92440c718e45b75727
notweezer123 <efentress@ucsd.edu>
2022-05-22 18:01:07 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -86,24 +86,22 @@ public class MarkdownParseTest {
     @Test
     public void testSnippet1() throws IOException {
         String contents= Files.readString(Path.of("./snippet-1.md"));
-        List<String> expect = List.of("url.com, google.com", "google.com", "ucsd.edu");
+        List<String> expect = List.of("google.com", "google.com", "ucsd.edu");
         assertEquals(expect, MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testSnippet2() throws IOException {
         String contents= Files.readString(Path.of("./snippet-2.md"));
-        List<String> expect = List.of("b.com", "a.com", "example.com");
+        List<String> expect = List.of("a.com", "a.com", "example.com");
         assertEquals(expect, MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testSnippet3() throws IOException {
         String contents= Files.readString(Path.of("./snippet-3.md"));
-        List<String> expect = List.of("https://www.twitter.com",
-            "https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule",
-            "github.com",
-            "https://cse.ucsd.edu/");
+        List<String> expect = List.of(
+            "https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
         assertEquals(expect, MarkdownParse.getLinks(contents));
     }
     // @Test
